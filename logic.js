293065d7b8f0599f10048497ee34433412825484
jewlmils <st.jewelmilagrosa@gmail.com>
2023-09-19 14:50:41 +0800

    added css and js

--- logic.js
+++ logic.js
@@ -1,1 +1,25 @@
-console.log('hello there!')
+const car = {
+    make: 'VW',
+    type: 'Polo',
+    seats: [
+        'seat1',
+        'seat2',
+        'seat3'
+    ],
+    isTurnedOn: false,
+    turnOn: function() { //method
+        this.isTurnedOn = true;
+    },
+    fly: function () {
+        alert('fly')
+    },
+    switchCar: function (isOn) {
+        if (isOn == true) {
+            this.isTurnedOn = true;
+        } else {
+            this.isTurnedOn = false;
+        }
+        console.log ('turn car ' +isOn)
+    }
+}
+
