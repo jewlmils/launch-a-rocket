dd31c673ad135350a91b92e366c40926f407c183
jewlmils <st.jewelmilagrosa@gmail.com>
2023-09-19 16:24:26 +0800

    made my rocket!

--- logic.js
+++ logic.js
@@ -1,25 +1,31 @@
-const car = {
-    make: 'VW',
-    type: 'Polo',
-    seats: [
-        'seat1',
-        'seat2',
-        'seat3'
-    ],
-    isTurnedOn: false,
-    turnOn: function() { //method
-        this.isTurnedOn = true;
-    },
-    fly: function () {
-        alert('fly')
-    },
-    switchCar: function (isOn) {
-        if (isOn == true) {
-            this.isTurnedOn = true;
-        } else {
-            this.isTurnedOn = false;
-        }
-        console.log ('turn car ' +isOn)
+let timer = null;
+let countdownNumber = 10;
+
+let changeState = function (state) {
+    document.body.className = 'body-state' + state;
+    clearInterval(timer);
+    countdownNumber = 10;
+    document.getElementById('countdown').innerHTML = countdownNumber;
+
+// countdown
+    if (state == 2) {
+        timer = setInterval(function () {
+            countdownNumber = countdownNumber - 1;
+            document.getElementById('countdown').innerHTML = countdownNumber;
+            if (countdownNumber <= 0) {
+                changeState(3)
+            }
+        }, 500);
+// sucess or failure
+    } else if (state === 3) {
+        let sucess = setTimeout(function () {
+            let randomNumber = Math.round(Math.random() * 10);
+            console.log('randomNumber: ', randomNumber)
+            if (randomNumber > 7) {
+                changeState(4) //yeheeey!
+            } else {
+                changeState(5) //nauuur :<
+            }
+        }, 2000);
     }
 }
-
